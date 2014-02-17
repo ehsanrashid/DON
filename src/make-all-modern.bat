e33c498231d54d150465b02a8676dd01d50c190a
Ehsan Rashid <ehsan.rashid@gmail.com>
2014-02-17 13:20:49 +0500

    fixing make file

--- src/make-all-modern.bat
+++ src/make-all-modern.bat
@@ -1,7 +1,7 @@
-ECHO OFF
-SET PATH=D:/MinGW/64/bin/;D:/MinGW/msys/bin/;
+@SET PATH=D:/MinGW/64/bin/;D:/MinGW/msys/bin/;
 
-REM make -f MakeFile build ARCH=x86-64-modern COMP=mingw config-sanity
-ECHO ON
+@REM make -f MakeFile build ARCH=x86-64-modern COMP=mingw config-sanity
+@REM make -f MakeFile build ARCH=x86-64-modern COMP=mingw
+
 make -f MakeFile build ARCH=x86-64-modern COMP=mingw
-PAUSE
+@PAUSE
--- src/make-all.bat
+++ src/make-all.bat
@@ -1,7 +1,8 @@
-ECHO OFF
-SET PATH=D:/MinGW/64/bin/;D:/MinGW/msys/bin/;
+@SET PATH=D:/MinGW/64/bin/;D:/MinGW/msys/bin/;
 
-REM make -f MakeFile build ARCH=x86-32 COMP=mingw config-sanity
-ECHO ON
+@REM make -f MakeFile build ARCH=x86-32 COMP=mingw config-sanity
+@REM make -f MakeFile build ARCH=x86-32 COMP=mingw
+@REM make -f MakeFile build ARCH=x86-64 COMP=mingw
+
 make -f MakeFile build ARCH=x86-32 COMP=mingw
-PAUSE
+@PAUSE
