7fcb828048cf6a950e2f446dcb3876f9b365e51d
Ehsan Rashid <ehsan.rashid@gmail.com>
2014-04-12 15:46:06 +0500

    make bat file changed

--- src/make-all-modern.bat
+++ src/make-all-modern.bat
@@ -1,13 +1,18 @@
 @SET PATH=D:/MinGW/64/bin/;D:/MinGW/msys/bin/;
 
 @REM make -f MakeFile build ARCH=x86-32-popcnt  COMP=mingw config-sanity
-@REM make -f MakeFile build ARCH=x86-64-popcnt  COMP=mingw config-sanity
 @REM make -f MakeFile build ARCH=x86-32-popcnt  COMP=mingw
+
+@REM make -f MakeFile build ARCH=x86-64-popcnt  COMP=mingw config-sanity
 @REM make -f MakeFile build ARCH=x86-64-popcnt  COMP=mingw
 
 @REM make -f MakeFile build ARCH=x86-64-bmi     COMP=mingw config-sanity
 @REM make -f MakeFile build ARCH=x86-64-bmi     COMP=mingw
 
-@REM make -f MakeFile profile-build ARCH=x86-64-popcnt  COMP=mingw
-make -f MakeFile profile-build ARCH=x86-64-bmi     COMP=mingw
+@REM make -f MakeFile build ARCH=x86-64-popcnt   COMP=mingw
+@REM make -f MakeFile build ARCH=x86-64-bmi      COMP=mingw
+
+make -f MakeFile profile-build ARCH=x86-64-popcnt  COMP=mingw
+@REM make -f MakeFile profile-build ARCH=x86-64-bmi     COMP=mingw
+
 @PAUSE
