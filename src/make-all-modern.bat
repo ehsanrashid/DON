3191c908475460da1ce9ad2a1909491dc7569790
Ehsan Rashid <ehsan.rashid@gmail.com>
2014-04-26 21:01:05 +0500

    typo

--- src/make-all-modern.bat
+++ src/make-all-modern.bat
@@ -6,13 +6,13 @@
 @REM make -f MakeFile build ARCH=x86-64-abm  COMP=mingw config-sanity
 @REM make -f MakeFile build ARCH=x86-64-abm  COMP=mingw
 
-@REM make -f MakeFile build ARCH=x86-64-bm2     COMP=mingw config-sanity
-@REM make -f MakeFile build ARCH=x86-64-bm2     COMP=mingw
+@REM make -f MakeFile build ARCH=x86-64-bm2  COMP=mingw config-sanity
+@REM make -f MakeFile build ARCH=x86-64-bm2  COMP=mingw
 
-@REM make -f MakeFile build ARCH=x86-64-abm   COMP=mingw
-@REM make -f MakeFile build ARCH=x86-64-bm2      COMP=mingw
+@REM make -f MakeFile build ARCH=x86-64-abm  COMP=mingw
+@REM make -f MakeFile build ARCH=x86-64-bm2  COMP=mingw
 
 make -f MakeFile profile-build ARCH=x86-64-abm  COMP=mingw
-@REM make -f MakeFile profile-build ARCH=x86-64-bm2     COMP=mingw
+@REM make -f MakeFile profile-build ARCH=x86-64-bm2  COMP=mingw
 
 @PAUSE
--- src/make-all.bat
+++ src/make-all.bat
@@ -9,6 +9,6 @@
 @REM make -f MakeFile profile-build ARCH=x86-32  COMP=mingw
 @REM make -f MakeFile profile-build ARCH=x86-64  COMP=mingw
 
-make -f MakeFile build ARCH=x86-64          COMP=mingw
+make -f MakeFile build ARCH=x86-64     COMP=mingw
 
 @PAUSE
