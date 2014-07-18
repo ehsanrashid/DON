c392b1130ac0b9cd61a838f03b40f202190094fa
Ehsan Rashid <ehsan.rashid@gmail.com>
2014-07-18 15:25:01 +0500

    fut margin

--- src/Searcher.cpp
+++ src/Searcher.cpp
@@ -1886,7 +1886,7 @@ namespace Searcher {
         {
             FutilityMoveCount[0][d] = u08 (2.40 + 0.222 * pow (0.00 + d, 1.80));
             FutilityMoveCount[1][d] = u08 (3.00 + 0.300 * pow (0.98 + d, 1.80));
-            FutilityMargin      [d] = Value (i32 ( 10 + (80 + 1*d)*d)); // Value (i32 ( 10 + 80*d));
+            FutilityMargin      [d] = Value (i32 ( 10 + (90 + 1*d)*d)); // Value (i32 ( 10 + 80*d));
             RazorMargin         [d] = Value (i32 (512 + 16*d));
         }
 
