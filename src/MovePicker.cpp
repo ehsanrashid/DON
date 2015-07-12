71118787d75aae88ea0261128027c1f21e8099b5
Ehsan Rashid <ehsan.rashid@gmail.com>
2015-07-12 18:06:08 +0500

    MVV/LVA in picker

--- src/MovePicker.cpp
+++ src/MovePicker.cpp
@@ -202,7 +202,8 @@ namespace MovePick {
             {
                 m.value = PIECE_VALUE[MG][mtype (m) == ENPASSANT && _Pos.en_passant_sq () == dst_sq (m) ? PAWN : ptype (_Pos[dst_sq (m)])]
                         + (mtype (m) == PROMOTE ? PIECE_VALUE[MG][promote (m)] - PIECE_VALUE[MG][PAWN] : VALUE_ZERO)
-                        - Value(ptype (_Pos[org_sq (m)])) + MAX_STATS_VALUE;
+                        - Value(10 * ptype (_Pos[org_sq (m)]))
+                        - Value(rel_rank (_Pos.active (), dst_sq (m))) + MAX_STATS_VALUE;
             }
             else
             {
