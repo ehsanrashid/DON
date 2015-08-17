aca42e8e4e2f87d99c6d14ddd0135bf24d52b00a
Ehsan Rashid <ehsan.rashid@gmail.com>
2015-08-18 01:34:31 +0500

    tt fix

--- src/Transposition.cpp
+++ src/Transposition.cpp
@@ -126,11 +126,11 @@ namespace Transposition {
         }
 
         auto *rte = fte;
-        Depth rem = (((0x100 + rte->gen () - _generation)&0xFC) + (rte->bound () == BOUND_EXACT))*2*DEPTH_ONE + rte->depth ();
+        auto rem = rte->depth () + ((rte->bound () == BOUND_EXACT) - ((0x100 + _generation - rte->gen ())&0xFC))*2*DEPTH_ONE;
         for (auto *ite = fte+1; ite < fte+ClusterEntryCount; ++ite)
         {
             // Implementation of replacement strategy when a collision occurs
-            Depth iem = (((0x100 + ite->gen () - _generation)&0xFC) + (ite->bound () == BOUND_EXACT))*2*DEPTH_ONE + ite->depth ();
+            auto iem = ite->depth () + ((ite->bound () == BOUND_EXACT) - ((0x100 + _generation - ite->gen ())&0xFC))*2*DEPTH_ONE;
             if (rem > iem)
             {
                 rem = iem;
