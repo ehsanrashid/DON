362fd0d6dfc8b65164909cc96494589da7ddf504
Ehsan Rashid <ehsan.rashid@gmail.com>
2020-09-15 19:02:42 +0500

    DelimitedIterator added

--- src/Helper.h
+++ src/Helper.h
@@ -36,3 +36,55 @@ namespace CommandLine {
     extern void initialize(int, char const *const[]);
 }
 
+
+/*
+template<class T, class Elem = char, class Traits = std::char_traits<Elem>>
+struct DelimitedIterator {
+
+public:
+    using iterator_category = std::output_iterator_tag;
+    using value_type = void;
+    using difference_type = void;
+    using pointer = void;
+    using reference = void;
+
+    using char_type = Elem;
+    using traits_type = Traits;
+    using ostream_type = std::basic_ostream<Elem, Traits>;
+
+    DelimitedIterator(ostream_type &os, const Elem *const delimiter) :
+        os(&os),
+        delimiter(delimiter),
+        first(true) {
+    }
+
+    DelimitedIterator &operator++() {
+        return *this;
+    }
+    DelimitedIterator &operator++(int) {
+        return *this;
+    }
+    DelimitedIterator &operator*() {
+        return *this;
+    }
+
+    DelimitedIterator &operator=(const T &t) {
+        if (first) {
+            first = false;
+        }
+        else {
+            if (delimiter) {
+                *os << delimiter;
+            }
+        }
+
+        *os << t;
+        return *this;
+    }
+
+protected:
+    ostream_type *os;
+    const Elem *delimiter;
+    bool first;
+};
+*/
