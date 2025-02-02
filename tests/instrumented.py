d0040e67dd3aff370b9c18e92bbe0a488c60f969
Ehsan Rashid <ehsan.rashid@gmail.com>
2025-02-02 11:35:09 +0500

    Update instrumented.py

--- tests/instrumented.py
+++ tests/instrumented.py
@@ -228,7 +228,7 @@ class TestInteractive(metaclass=OrderedClassMembers):
         self.engine.send_command("go depth 5")
 
         def callback(output):
-            regex = r"info depth \d+ seldepth \d+ multipv \d+ score cp \d+ nodes \d+ nps \d+ hashfull \d+ tbhits \d+ time \d+ pv"
+            regex = r"info depth \d+ seldepth \d+ multipv \d+ score cp \d+ time \d+ nodes \d+ nps \d+ hashfull \d+ tbhits \d+ pv"
             if output.startswith("info depth") and not re.match(regex, output):
                 assert False
             if output.startswith("bestmove"):
@@ -248,7 +248,7 @@ class TestInteractive(metaclass=OrderedClassMembers):
         def callback(output):
             nonlocal depth
 
-            regex = rf"info depth {depth} seldepth \d+ multipv \d+ score cp \d+ wdl \d+ \d+ \d+ nodes \d+ nps \d+ hashfull \d+ tbhits \d+ time \d+ pv"
+            regex = rf"info depth {depth} seldepth \d+ multipv \d+ score cp \d+ wdl \d+ \d+ \d+ time \d+ nodes \d+ nps \d+ hashfull \d+ tbhits \d+ pv"
 
             if output.startswith("info depth"):
                 if not re.match(regex, output):
