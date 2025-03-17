eb3727ef50728781d0a8852aeb4050482b3c4fce
Ehsan Rashid <ehsan.rashid@gmail.com>
2025-03-18 04:01:27 +0500

    Simplify Deep search

--- src/search.cpp
+++ src/search.cpp
@@ -1383,12 +1383,11 @@ S_MOVES_LOOP:  // When in check, search starts here
             if (value > alpha && newDepth > redDepth)
             {
                 // Adjust full-depth search based on LMR value
-                if (!is_decisive(value) && !is_decisive(bestValue))
-                    newDepth +=
-                      // - if the value was good enough search deeper
-                      +(value > 43 + bestValue + 2 * newDepth)
-                      // - if the value was bad enough search shallower
-                      - (value < 9 + bestValue);
+                newDepth +=
+                  // - if the value was good enough search deeper
+                  +(value > 43 + bestValue + 2 * newDepth)
+                  // - if the value was bad enough search shallower
+                  - (value < 9 + bestValue);
 
                 if (newDepth > redDepth)
                     value = -search<~NT>(pos, ss + 1, -(alpha + 1), -alpha, newDepth);
