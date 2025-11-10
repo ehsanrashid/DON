f4eacb9905485aa2c3ce2b2de69eb4b7bf548e89
Ehsan Rashid <ehsan.rashid@gmail.com>
2025-11-10 13:40:32 +0500

    ...

--- src/search.cpp
+++ src/search.cpp
@@ -934,7 +934,7 @@ Value Worker::search(Position&    pos,
         assert((ss - 1)->move != Move::Null);
 
         // Null move dynamic reduction based on depth and phase
-        Depth R = 6 + depth / 3 + pos.phase() / 9 + improve;
+        Depth R = 5 + depth / 3 + pos.phase() / 9 + improve;
 
         do_null_move(pos, st, ss);
 
