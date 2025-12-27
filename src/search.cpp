57000ff7c8fdaa407d410455e24aa6c37bd8b7b4
Ehsan Rashid <ehsan.rashid@gmail.com>
2025-12-27 19:27:04 +0500

    matetrack fix
    Bench: 2711840

--- src/search.cpp
+++ src/search.cpp
@@ -2039,7 +2039,7 @@ int Worker::correction_value(const Position& pos, const Stack* const ss) noexcep
                      + histories.    pawn_correction<BLACK>(pos.    pawn_key(BLACK))[ac])
            + 4411LL * (histories.   minor_correction<WHITE>(pos.   minor_key(WHITE))[ac]
                      + histories.   minor_correction<BLACK>(pos.   minor_key(BLACK))[ac])
-           +11665LL * (histories.non_pawn_correction<WHITE>(pos.non_pawn_key(WHITE))[ac]
+           +11530LL * (histories.non_pawn_correction<WHITE>(pos.non_pawn_key(WHITE))[ac]
                      + histories.non_pawn_correction<BLACK>(pos.non_pawn_key(BLACK))[ac])
            + 7841LL * (is_ok(preSq)
                       ? (*(ss - 2)->pieceSqCorrectionHistory)[pos[preSq]][preSq]
