0c1e1d557a5a2d3f9f985642813cc0915a71dc72
Ehsan Rashid <ehsan.rashid@gmail.com>
2026-01-12 00:57:06 +0500

    Update search.cpp
    Bench: 2296907

--- src/search.cpp
+++ src/search.cpp
@@ -1425,7 +1425,8 @@ S_MOVES_LOOP:  // When in check, search starts here
         r += int(ttCapture) * 1119;
 
         // Increase reduction if next ply has a lot of fail high
-        r += int(ss->cutoffCount > 1) * ((256 << (ss->cutoffCount - 2)) + int(AllNode) * 1024);
+        r += int(ss->cutoffCount > 1)
+           * (256 * std::pow(1.98, ss->cutoffCount - 2) + int(AllNode) * 1024);
 
         // For first picked move (ttMove) reduce reduction
         r -= int(ttm) * 2151;
