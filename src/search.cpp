2c8bd3da9fc54e1b71cebb3dd6b4fbe27efaad03
Ehsan Rashid <ehsan.rashid@gmail.com>
2026-01-26 23:13:12 +0500

    update margin
    Bench: 2498104

--- src/search.cpp
+++ src/search.cpp
@@ -1065,7 +1065,7 @@ Value Worker::search(Position& pos, Stack* const ss, Value alpha, Value beta, De
     {
         if (!ss->ttPv && !exclude && depth < 14
             && !is_win(ttEvalValue) && !is_loss(beta)
-            && (ttmNone || history_value(pos, ttd.move, ac, contHistory) >= (ttmCapture ? 1024 : 65536)))
+            && (ttmNone || history_value(pos, ttd.move, ac, contHistory) >= (ttmCapture ? 2048 : 65536)))
         {
             // Compute base futility
             int baseFutility = 53 + int(ttd.hit) * 23;
