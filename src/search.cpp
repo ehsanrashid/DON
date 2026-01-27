15dc29ecded9f2d4899595ef3727ce00c2ff97cd
Ehsan Rashid <ehsan.rashid@gmail.com>
2026-01-27 17:09:50 +0500

    remove abs()
    Bench: 2310301

--- src/search.cpp
+++ src/search.cpp
@@ -1062,7 +1062,7 @@ Value Worker::search(Position& pos, Stack* const ss, Value alpha, Value beta, De
     {
         // The depth condition is important for mate finding
         if (!ss->ttPv && !exclude && depth < 14 && !is_win(ttEvalValue) && !is_loss(beta)
-            && (ttmNone || std::abs(history_value(pos, ttd.move, ac, contHistory)) >= (ttmCapture ? 8192 : 32768)))
+            && (ttmNone || history_value(pos, ttd.move, ac, contHistory) >= (ttmCapture ? 8192 : 32768)))
         {
             // Compute base futility
             int baseFutility = 53 + int(ttd.hit) * 23;
