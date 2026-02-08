1761c0caf0393fbea9709ba80150cee502a662d6
Ehsan Rashid <ehsan.rashid@gmail.com>
2026-02-08 15:59:07 +0500

    matetrack fix
    Bench: 2371804

--- src/search.cpp
+++ src/search.cpp
@@ -2214,7 +2214,7 @@ int Worker::correction_value(const Position& pos, const Stack* ss) noexcept {
     Color ac = pos.active_color();
 
     std::int64_t correctionValue =
-           + 5174LL * (histories.    pawn_correction<WHITE>(pos.    pawn_key(WHITE))[ac]
+           + 5173LL * (histories.    pawn_correction<WHITE>(pos.    pawn_key(WHITE))[ac]
                      + histories.    pawn_correction<BLACK>(pos.    pawn_key(BLACK))[ac])
            + 4411LL * (histories.   minor_correction<WHITE>(pos.   minor_key(WHITE))[ac]
                      + histories.   minor_correction<BLACK>(pos.   minor_key(BLACK))[ac])
