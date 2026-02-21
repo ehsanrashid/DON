b6b53921397387361b1d0218f97dfb79c5ffc669
Ehsan Rashid <ehsan.rashid@gmail.com>
2026-02-21 14:30:22 +0500

    Update search.cpp
    Bench: 2483407

--- src/search.cpp
+++ src/search.cpp
@@ -1632,8 +1632,8 @@ Value Worker::search(Position& pos, Stack* ss, Value alpha, Value beta, Depth de
                 alpha = value;  // Update alpha! Always alpha < beta
 
                 // Reduce depth for other moves if have found at least one score improvement
-                if (depth < 16 && !is_decisive(value))
-                    depth = std::max(depth - 1 - int(depth < 12), 1);
+                if (depth < 20 && !is_decisive(value))
+                    depth = std::max(depth - 1 - int(depth < 14), 1);
             }
         }
 
