7ab1ec1bdd92a19efc51600a7b7c3ed592d16095
Ehsan Rashid <ehsan.rashid@gmail.com>
2026-02-21 09:01:29 +0500

    Update search.cpp
    Bench: 2409830

--- src/search.cpp
+++ src/search.cpp
@@ -2151,9 +2151,9 @@ int Worker::correction_value(const Position& pos, const Stack* ss) noexcept {
     Color ac = pos.active_color();
 
     std::int64_t correctionValue =
-           + 5717LL * int(histories.    pawn_correction<WHITE>(pos.    pawn_key(WHITE))[ac]
+           + 5716LL * int(histories.    pawn_correction<WHITE>(pos.    pawn_key(WHITE))[ac]
                         + histories.    pawn_correction<BLACK>(pos.    pawn_key(BLACK))[ac])
-           + 4412LL * int(histories.   minor_correction<WHITE>(pos.   minor_key(WHITE))[ac]
+           + 4411LL * int(histories.   minor_correction<WHITE>(pos.   minor_key(WHITE))[ac]
                         + histories.   minor_correction<BLACK>(pos.   minor_key(BLACK))[ac])
            +12749LL * int(histories.non_pawn_correction<WHITE>(pos.non_pawn_key(WHITE))[ac]
                         + histories.non_pawn_correction<BLACK>(pos.non_pawn_key(BLACK))[ac]);
