54e91a9289b5b39861521c3b423a71773986da04
Ehsan Rashid <ehsan.rashid@gmail.com>
2026-02-27 05:04:58 +0500

    Final DON
    Bench: 1923607

--- src/engine.cpp
+++ src/engine.cpp
@@ -101,10 +101,11 @@ Engine::Engine(std::string_view path) noexcept :
     options.add("UCI_ELO",              Option(Skill::ELO_MAX, Skill::ELO_MIN, Skill::ELO_MAX));
     options.add("UCI_ShowWDL",          Option(false));
     options.add("SkillLevel",           Option(int(Skill::LEVEL_MAX), int(Skill::LEVEL_MIN), int(Skill::LEVEL_MAX)));
-    options.add("OverheadTime",         Option(25,  0, 5000));  // Overhead per move
-    options.add("MinimumMoveTime",      Option(20,  0, 5000));  // Time floor constraint
-    options.add("BufferTime",           Option(10,  0, 5000));  // Safety reserve (very intuitive)
-    options.add("TimePercent",          Option(80, 10, 1000));  // Time scaling factor (%age)
+    options.add("OverheadTime",         Option(25,  0, 5000));  // Estimated overhead per move
+    options.add("MinMoveTime",          Option(20,  0, 5000));  // Minimum time allowed per move
+    options.add("MaxSingleTime",        Option(512, 0, 5000));  // Maximum time allowed for a single move
+    options.add("BufferTime",           Option(10,  0, 5000));  // Safety reserve to prevent time trouble
+    options.add("TimePercent",          Option(80, 10, 1000));  // Percentage of remaining time to use
     options.add("NodesTime",            Option(0, 0, 10000));
     options.add("SleepOnStart",         Option(false));
     options.add("DrawMoveCount",        Option(Position::DrawMoveCount, 5, 50, OnCng([](const Option& o) { Position::DrawMoveCount = int(o); return std::nullopt; })));
