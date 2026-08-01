bc751ea4d1a0d1c4512551a007f925e10d00fa48
Ehsan Rashid <ehsan.rashid@gmail.com>
2026-08-01 17:52:39 +0500

    Final Fix
    Bench: 2369051

--- src/search.cpp
+++ src/search.cpp
@@ -47,8 +47,10 @@ constexpr Depth OutputLimitDepth = 30;
 
 constexpr double BetaBias = 0.67;
 
-constexpr Array<int, 16> LMRDivisor{3637, 2787, 2761, 2939, 3171, 3347, 3147, 2762,
-                                    2772, 3106, 3107, 3060, 3112, 2991, 3090, 3542};
+constexpr Array<int, 16> LMRDivisor{
+  3637, 2787, 2761, 2939, 3171, 3347, 3147, 2762,  //
+  2772, 3106, 3107, 3060, 3112, 2991, 3090, 3542   //
+};
 
 // Reductions lookup table using [depth or moveCount]
 alignas(CACHE_LINE_SIZE) constexpr auto Reductions = []() constexpr noexcept {
