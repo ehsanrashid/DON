1df48e3db347452152844de24bc6698897b9c7dc
Ehsan Rashid <ehsan.rashid@gmail.com>
2026-08-05 23:17:04 +0500

    net.sh fix
    Bench: 2350718

--- scripts/net.sh
+++ scripts/net.sh
@@ -2,8 +2,8 @@
 
 # Download commands with a 5min time-out to ensure things fail if the server stalls
 wget_or_curl=$(
-    command -v wget >/dev/null 2>&1 && echo "wget -qO- --timeout=300 --tries=1" ||
-    command -v curl >/dev/null 2>&1 && echo "curl -skL --max-time 300"
+    (command -v wget >/dev/null 2>&1 && echo "wget -qO- --timeout=300 --tries=1") ||
+    (command -v curl >/dev/null 2>&1 && echo "curl -skL --max-time 300")
 )
 
 if [ -z "$wget_or_curl" ]; then
@@ -12,8 +12,8 @@ if [ -z "$wget_or_curl" ]; then
 fi
 
 sha256sum=$(
-    command -v shasum >/dev/null 2>&1 && echo "shasum -a 256" ||
-    command -v sha256sum >/dev/null 2>&1 && echo "sha256sum"
+    (command -v shasum >/dev/null 2>&1 && echo "shasum -a 256") ||
+    (command -v sha256sum >/dev/null 2>&1 && echo "sha256sum")
 )
 
 if [ -z "$sha256sum" ]; then
@@ -55,10 +55,10 @@ fetch_network() {
                     "Install one of these tools to download NNUE files automatically."
         exit 1
     fi
-
-    for url in \
-    "https://tests.stockfishchess.org/api/nn/$filename" \
-    "https://github.com/official-stockfish/networks/raw/master/$filename"; do
+    printf 'wget_or_curl=[%s]\n' "$wget_or_curl"
+    for url in "https://tests.stockfishchess.org/api/nn/$filename" \
+               "https://github.com/official-stockfish/networks/raw/master/$filename"
+    do
         echo "Downloading from $url ..."
         if $wget_or_curl "$url" >"$filename"; then
             if validate_network "$filename"; then
