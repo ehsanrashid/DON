86b0fac2a654531750d89d3e24564cf5616c88f6
Ehsan Rashid <ehsan.rashid@gmail.com>
2026-08-08 22:57:44 +0500

    Final DON
    Bench: 2678386

--- src/nnue/simd.h
+++ src/nnue/simd.h
@@ -224,9 +224,13 @@ inline constexpr u32 Mask[4]{1, 2, 4, 8};
     #define MaxChunkSize 16
 
     #if defined(__arm__) && !defined(__aarch64__)
-// Instructions that don't exist on 32-bit ARM
-inline int16x8_t vaddw_high_s8(int16x8_t a, int8x16_t b) { return vaddw_s8(a, vget_high_s8(b)); }
-inline int16x8_t vsubw_high_s8(int16x8_t a, int8x16_t b) { return vsubw_s8(a, vget_high_s8(b)); }
+// NEON intrinsics unavailable on 32-bit ARM
+inline int16x8_t vaddw_high_s8(int16x8_t a, int8x16_t b) noexcept {
+    return vaddw_s8(a, vget_high_s8(b));
+}
+inline int16x8_t vsubw_high_s8(int16x8_t a, int8x16_t b) noexcept {
+    return vsubw_s8(a, vget_high_s8(b));
+}
     #endif
 
 #else
