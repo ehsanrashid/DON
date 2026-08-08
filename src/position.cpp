593d380189d6f13d351e44343e36374bf1a71a97
Ehsan Rashid <ehsan.rashid@gmail.com>
2026-08-09 01:57:24 +0500

    Bug fix
    Bench: 2405297

--- src/position.cpp
+++ src/position.cpp
@@ -966,7 +966,8 @@ DirtyBoard Position::do_move(const Move          m,
                 assert(st->preSt->rule50Count == 0);
 
                 // Remove the captured pawn
-                remove(capturedSq);
+                remove(capturedSq, &db.dirtyThreats);
+                move(orgSq, dstSq, &db.dirtyThreats);
 
                 capturedKey = Zobrist::piece_square(~ac, capturedPt, capturedSq);
             }
@@ -976,6 +977,12 @@ DirtyBoard Position::do_move(const Move          m,
         else
         {
             st->nonPawnKeys[~ac][is_major(capturedPt)] ^= capturedKey;
+        }
+
+        if (m.type() != MT::PROMOTION && m.type() != MT::EN_PASSANT)
+        {
+            remove(orgSq, &db.dirtyThreats);
+            swap(dstSq, movedPc, &db.dirtyThreats);
         }
 
         db.dirtyPiece.removedSq = capturedSq;
@@ -988,15 +995,10 @@ DirtyBoard Position::do_move(const Move          m,
         // Reset rule 50 draw counter
         reset_rule50_count();
     }
-
-    if (capture && m.type() != MT::EN_PASSANT)
-    {
-        remove(orgSq, &db.dirtyThreats);
-        swap(dstSq, movedPc, &db.dirtyThreats);
-    }
     else
     {
-        move(orgSq, dstSq, &db.dirtyThreats);
+        if (m.type() != MT::PROMOTION)
+            move(orgSq, dstSq, &db.dirtyThreats);
     }
 
     // If the moving piece is a pawn do some special extra work
@@ -1017,7 +1019,12 @@ DirtyBoard Position::do_move(const Move          m,
             db.dirtyPiece.addedSq = dstSq;
             db.dirtyPiece.addedPc = promotedPc;
 
-            swap(dstSq, promotedPc, &db.dirtyThreats);
+            remove(orgSq, &db.dirtyThreats);
+
+            if (capture)
+                swap(dstSq, promotedPc, &db.dirtyThreats);
+            else
+                put(dstSq, promotedPc, &db.dirtyThreats);
 
             assert(count(promotedPc) != 0);
             assert(Zobrist::piece_square(ac, PAWN, dstSq) == 0);
