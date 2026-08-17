64bd15793d2d7a486ffe16ff7402ffcb05bb572f
Ehsan Rashid <ehsan.rashid@gmail.com>
2026-08-17 21:35:36 +0500

    Update Makefile

--- src/nnue/simd.h
+++ src/nnue/simd.h
@@ -137,13 +137,15 @@ using vec_uint_t = __m128i;
     #define vec_load(src) (*(src))
     #define vec_store(dst, value) *(dst) = (value)
 
-    #if defined(__i386__)
-inline __m128i _mm_cvtsi64_si128(const i64 value) noexcept {
+    #if defined(USE_SSE41)     // SSE4.1 enabled?
+        #if defined(__i386__)  // 32-bit x86?
+inline __m128i i386_cvtsi64_si128(const i64 value) noexcept {
     return _mm_loadl_epi64(reinterpret_cast<const __m128i*>(&value));
 }
-    #endif
-    #if defined(USE_SSE41)
-        #define vec_convert_8_16(a) _mm_cvtepi8_epi16(_mm_cvtsi64_si128(i64(a)))
+            #define vec_convert_8_16(a) _mm_cvtepi8_epi16(SIMD::i386_cvtsi64_si128(i64(a)))
+        #else
+            #define vec_convert_8_16(a) _mm_cvtepi8_epi16(_mm_cvtsi64_si128(i64(a)))
+        #endif
     #else
 inline __m128i vec_convert_8_16(const u64 a) noexcept {
     const __m128i v8   = _mm_cvtsi64_si128(i64(a));
