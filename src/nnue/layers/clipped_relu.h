f03268ac2dce99fa6eca97d29ea36d1ed843878b
Ehsan Rashid <ehsan.rashid@gmail.com>
2026-08-31 23:29:17 +0500

    Update clipped_relu.h

--- src/nnue/layers/clipped_relu.h
+++ src/nnue/layers/clipped_relu.h
@@ -163,7 +163,7 @@ class ClippedReLU final {
             in             = __riscv_vmax_vx_i32m4(in, 0, vl);
 
             vint16m2_t words =
-              __riscv_vnclip_wx_i16m2(in, WeightScaleBitsLocal, __RISCV_VXRM_RDN, vl);
+              __riscv_vnclip_wx_i16m2(in, WeightScaleBits, __RISCV_VXRM_RDN, vl);
             vint8m1_t narrowed = __riscv_vnclip_wx_i8m1(words, 0, __RISCV_VXRM_RDN, vl);
 
             __riscv_vse8_v_u8m1(&output[j], __riscv_vreinterpret_v_i8m1_u8m1(narrowed), vl);
