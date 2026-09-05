169d0d952de2cbf4e26c4c22e612d85f5792b8fc
Ehsan Rashid <ehsan.rashid@gmail.com>
2026-09-05 22:35:44 +0500

    Final DON
    Bench: 2197768

--- src/main.cpp
+++ src/main.cpp
@@ -42,15 +42,12 @@ int main(int argc, const char* argv[]) noexcept {
     set_console_output(ConsoleMode::UTF8);
 
     std::cout << engine_info() << std::endl;
-
     std::cout << timestamp() << std::endl;
 
     show_logo();
 
     Attacks::init();
-
     Position::init();
-
     Tablebase::Syzygy::init();
 
     CommandLine commandLine(argc, argv);
