234367cc8bc7e89e34be49ffc516fd570ab7de63
Ehsan Rashid <ehsan.rashid@gmail.com>
2026-09-23 14:22:00 +0500

    Final DON

--- src/shm.cpp
+++ src/shm.cpp
@@ -30,6 +30,9 @@
     #include <cstdlib>       // atexit()
     #include <mutex>         // lock_guard
     #include <shared_mutex>  // shared_lock, shared_mutex
+
+#elif defined(__ANDROID__)
+    #include <unistd.h>
 #endif
 
 namespace DON {
@@ -96,7 +99,7 @@ std::string executable_path() noexcept {
         executablePath[executableSize] = '\0';
     }
 
-#elif defined(__linux__) && !defined(__ANDROID__)
+#elif defined(__linux__) || defined(__ANDROID__)
     ssize_t size =  //
       ::readlink("/proc/self/exe", executablePath.data(), executablePath.size() - 1);
 
@@ -107,8 +110,6 @@ std::string executable_path() noexcept {
     }
 
 #elif defined(__wasm__)
-
-#elif defined(__ANDROID__)
 
 #else
     #error "Unsupported platform"
